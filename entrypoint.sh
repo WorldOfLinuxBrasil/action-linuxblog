1b2a59626fbe12d9ee4159d541643511fcdf348f
Lin Diniz <gabrielvdiniz@gmail.com>
2020-12-17 16:46:30 +0000

    a

--- entrypoint.sh
+++ entrypoint.sh
@@ -73,6 +73,6 @@ echo 'Start Commit'
 git commit --allow-empty -m "Deploying to ${BRANCH}"
 
 echo 'Start Push'
-git push origin git@github.com:WorldOfLinuxBrasil/linuxblog.git "${BRANCH}" --force
+git push git@github.com:WorldOfLinuxBrasil/linuxblog.git "${BRANCH}" --force
 
 echo "Deployment succesful!"
